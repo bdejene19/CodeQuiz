c2e6366330cc2a5b7dafdffa28e9cd29a03c9efe
Bemnet Dejene <bdejene19@gmail.com>
2022-01-25 16:35:16 -0500

    finished all logic. need to run some basic testing to makesure workflow is proper and handles edgecases

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,11 @@
 // when quiz starts, create containers questions and answers;
-
+let highScoresOpen = false;
+let userScore = 0;
 let questions = {
     q1: {
         question: 'Forgetting a bracket in your code will lead to a ____ error',
         possibleAnswers: ['Syntax', 'Run-Time', 'Logic', 'Compilation'],
-        correctAnswer: 'Compilation',
+        answer: 'Compilation',
     },
 
     q2: {
@@ -31,73 +32,156 @@ let questions = {
         answer: 'git remote',
     },
 }
+let timeLeft = document.getElementById('timeLeft');
+let startBtn = document.getElementById('start-btn');
+let timer = 90;
 let slideValue = 0;
-let userAnswers = [];
+// slides game board to the right for next question
+const calculateScore = () => {
+    let additionalPoints = 0;
 
-const submitAnswer = (event) => {
-    let submissionValue = event.target.value;
-    userAnswers.push(submissionValue);
+    if (timer > 0) {
+        additionalPoints =  timer * 50
 
-    let gameContainer = document.querySelectorAll('.question');
-    slideValue -= 100;
-    console.log(slideValue);
-    gameContainer.forEach(quest => quest.style.cssText= `transform: translateX(${slideValue}%); transition: 0.5s ease-in-out;`);
+    } else {
+        additionalPoints = 0;
+    }
+    userScore += additionalPoints;
+    return userScore;
+}
+const displayScore = () => {
+    document.getElementById('game-content').style.display = 'none';
+
+    let highScoreContainer = document.getElementById('highscore-container');
+    let showUserScore = document.getElementById('user-score');
+
+    showUserScore.textContent = calculateScore();
+    highScoreContainer.style.display = 'block';
 
 }
+const slideGameBoardLeft = () => {
+    slideValue -= 100;
+    let gameContainer = document.querySelectorAll('.question');
+    gameContainer.forEach(quest => quest.style.cssText= `transform: translateX(${slideValue}%); transition: 1s ease-in-out;`);
+}
+const submitAnswer = (event) => {
+    let parentEl = event.target.parentElement;
+    let submissionValue = event.target.value;
+
+    let questionAnswer = parentEl.dataset.answer
+    let userAnswer = event.target;
+
+    let commonStyling = 'opacity: 0.8; font-weight: 700'
+    if (submissionValue === questionAnswer) {
+        userAnswer.style.cssText = `background-color: lime; ${commonStyling} border: lime;`
+      
+        userScore += 100;
+    } else {
+        userAnswer.style.cssText = `background-color: red; ${commonStyling} border: red;`;
+        if (timer - 10 <= 0) {
+            timer = 0;
+        } else {
+            timer -= 10;
+        }
+        timeLeft.style.color = 'red';
+
+        setTimeout(() => {
+            timeLeft.style.color = 'white';
+        }, 2000)
+    }
+
+    setTimeout(() => {
+        userAnswer.style.cssText = `background-color: ##7900FF; ${commonStyling} border: inherit;`;
+
+    }, 2000)
+
+    slideGameBoardLeft();
+}
+
+
 const createQuestions = () => {
     let game = document.getElementById('game-content')
    
     Object.entries(questions).forEach(item => {
+        // create container for question
         const questionContainer = document.createElement('article');
         questionContainer.setAttribute('class', 'question');
-    
+        
+        // create header and ol containers for question title and possible answers
         let questionTitle = document.createElement('h2');
         let possibleAnswersContainer = document.createElement('ol');
 
+        // store answer as attribute in ol holding possible answers for question
+        possibleAnswersContainer.setAttribute('data-answer', item[1].answer);
+
+        // set h2 content for question => question number and question
         questionTitle.textContent = `${item[0].toUpperCase()}. ${item[1].question}`;
+
+        // get potential answers for question and create button for each possible answer
         let answers = item[1].possibleAnswers;
 
+        // loop through possible answers
         answers.forEach((questionAnswers,index) => {
-            
+            // creating button
             let newListItem = document.createElement('button');
+            // set class and value attributes for button
             newListItem.setAttribute('class', 'answer-choice')
             newListItem.setAttribute('value', `${questionAnswers}`)
 
+            // create question answer text for button
             newListItem.textContent = `${index + 1}. ${questionAnswers}`;
+
+            // append to ol
             possibleAnswersContainer.appendChild(newListItem);
 
-            newListItem.addEventListener('click', submitAnswer)
+            // create click events for answers => could be refactored using delegation
+            newListItem.addEventListener('click',submitAnswer)
 
         })
-        
 
-
+        // append question and possible answers to question container
         questionContainer.appendChild(questionTitle);
         questionContainer.appendChild(possibleAnswersContainer);
+
+        // append questioncontainer to DOM
         game.appendChild(questionContainer);
 
     })
 
 }
 
-let timeLeft = document.getElementById('timeLeft');
-let startBtn = document.getElementById('start-btn');
+const resetGame = () => {
+    timer = 90;
+    slideValue = 0;
+    highScoresOpen = false;
 
-const startQuiz = () => {
+    let gameContainer = document.querySelectorAll('.question');
+    gameContainer.forEach(quest => quest.style.cssText= `transform: translateX(${slideValue}); transition: 1s ease-in-out;`);
+    document.getElementById('game-content').style.display = 'flex';
+}
+const hideBgContent = () => {
     let homeContent = document.getElementById('quiz-home');
-    // let timeLeft = document.getElementById('');
+
     homeContent.style.display = 'none';
+}
+const startQuiz = () => {
+    resetGame();
+    hideBgContent();
     createQuestions();
-    let timer = 90;
 
     let quizStarted = setInterval(() => {
         // console.log('started interval')
-        if (timer > 0) {
-            // console.log('entered if statement');
-            timeLeft.textContent = timer;
+        if (slideValue === (-100 * Object.keys(questions).length) || timer < 0) {
+            displayScore();
+            clearInterval(quizStarted);
+        } else  if (timer >= 0) {
+
+            timeLeft.textContent = timer;            
             timer--;
 
         } else {
+            console.log('hitting else statement');
+            displayScore();
             clearInterval(quizStarted);
         }
     }, 1000)
@@ -107,8 +191,110 @@ const startQuiz = () => {
 startBtn.addEventListener('click', startQuiz)
 
 
+const saveScoreToLocalStorage = () => {
+    let gamerId = document.querySelector('#gamer-tag').value;
+    let userNameAndScore = {
+        userName: gamerId,
+        score: userScore,
+    }    
+    if (localStorage.getItem('past-players')) {
+        let pastPlayers = localStorage.getItem('past-players');
+        let pastPlayersJSON = JSON.parse(pastPlayers);
+        pastPlayersJSON.push(userNameAndScore);
+        localStorage.setItem('past-players', JSON.stringify(pastPlayersJSON))
+        console.log(localStorage.getItem('past-players'))
+    } 
+
+
+}
+const displayLeaderBoards = () => {
+    let allPlayers = localStorage.getItem('past-players');
+    let allPlayersList = JSON.parse(allPlayers);
+    let sortedPlayerList = allPlayersList.sort((a,b) => {
+        if (a.score > b.score) {
+            return -1;
+        }
+    })
+
+    let allScores = document.getElementById('all-scores');
+
+    sortedPlayerList.forEach((item,index)  => {
+        let newListItem = document.createElement('li');
+
+        if (index < 3) {
+            newListItem.setAttribute('class', 'medalWinner')
+        }
+        newListItem.textContent = `${index + 1}. ${item.userName}: ${item.score}`;
+        allScores.append(newListItem);
+    })
+
+    allScores.style.display = 'flex';
+
+    let scoreForm = document.getElementById('scoreSubmission');
+    scoreForm.style.display = 'none';
+
+    highScoresOpen = true;
+
+}
+const submitScore = (event) => {
+
+    event.preventDefault();
+
+    let gamerId = document.querySelector('#gamer-tag').value;
+
+    console.log('error')
+
+    if (gamerId === "") {
+        document.querySelector('#gamer-tag').style.border = 'solid red 3px'
+    } else {
+        saveScoreToLocalStorage();
+            // retrieve local storage
+        displayLeaderBoards();
+    }
+
+   
+}
 
 
 
+let scoreSubmission = document.getElementById('submit-score');
+scoreSubmission.addEventListener('click', submitScore)
 
+    let viewScores = document.getElementById('viewHighscores');
+
+const viewHighScores = () => {
+    let quizHome = document.getElementById('quiz-home');
+    let scoresContainer = document.getElementById('highscore-container');
+
+    if (!highScoresOpen) {
+        scoresContainer.style.display = 'block';
+        quizHome.style.display = 'none';
+        displayLeaderBoards();
+    } else {
+        quizHome.style.display = 'flex';
+        scoresContainer.style.display = 'none';
+
+    }
+
+    highScoresOpen = !highScoresOpen   
+}
+
+viewScores.addEventListener('click', viewHighScores)
+
+const navigateHome = () => {
+    let scoresContainer = document.getElementById('highscore-container');
+    let allScores = document.getElementById('all-scores');
+
+    allScores.style.display = 'none';
+    scoresContainer.style.display = 'none';
+
+    let quizHome = document.getElementById('quiz-home');
+    quizHome.style.display = 'flex';
+
+
+}
+
+let goBack = document.getElementById('go-back')
+
+goBack.addEventListener('click', navigateHome)
 
